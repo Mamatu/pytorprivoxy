2005b93f63d0c87fdcdff291281c0650d01e1b4b
Marcin Matula <marcin.matula@gmail.com>
2024-03-14 17:34:56 +0100

    using multiprocess server instead fifo named pipe

--- main.py
+++ main.py
@@ -16,40 +16,32 @@ def _instances_remove(instance):
 def start(socks_port, control_port, listen_port, wait_for_initialization = True, **kwargs):
     global __instances
     callback_before_wait = lambda instance: __instances_append(instance)
-    instance = lib.start(socks_port, control_port, listen_port, callback_before_wait = callback_before_wait, wait_for_initialization = wait_for_initialization, **kwargs)
+    return lib.start(socks_port, control_port, listen_port, callback_before_wait = callback_before_wait, wait_for_initialization = wait_for_initialization, **kwargs)
     return instance
 
 def start_multiple(ports : list, wait_for_initialization = True, **kwargs):
     global __instances
     callback_before_wait = lambda instance: _instances_append(instance)
-    instances = lib.start_multiple(ports, callback_before_wait = callback_before_wait, wait_for_initialization = wait_for_initialization, **kwargs)
+    return lib.start_multiple(ports, callback_before_wait = callback_before_wait, wait_for_initialization = wait_for_initialization, **kwargs)
     return instances
 
 def stop(instance):
     lib.stop(instance)
     _instances_remove(instance)
 
-_named_fifo_path = None
-
-def stop_named_fifo():
-    global _named_fifo_path
-    if _named_fifo_path:
-        import os, stat
-        if os.path.exists(_named_fifo_path) and stat.S_ISFIFO(os.stat(_named_fifo_path).st_mode):
-            os.system(f"echo \"stop\" > {_named_fifo_path}")
-            os.remove(_named_fifo_path)
-
 def stop_all():
     global __instances
-    stop_named_fifo()
     for i in __instances: stop(i)
 
 import atexit
 atexit.register(stop_all)
+server = None
 
 import signal
 def signal_handler(sig, frame):
+    global server
     log.info("Registering sig for application. It will stop all")
+    if server is not None: server.stop()
     stop_all()
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -63,7 +55,7 @@ if __name__ == "__main__":
     parser.add_argument("--stdout", help = "logging into stdout", action='store_true')
     parser.add_argument("--timeout", help = "timeout for initialization, in the seconds. Default: 300s", type=int, default=300)
     parser.add_argument("--password_from_file", help = "Load password from file", type=str, default=None)
-    parser.add_argument("--mkfifo", help = "Make named pipe to comunicate with privoxy", type=str, default="/tmp/privoxy.fifo")
+    parser.add_argument("--server", help = "Establish server to multiprocess communication. As argument it takes listen port", type=int, default=7000)
     factor_description = """
     Factor of success of initialization after what the app will be continued, otherwise it will interrupted.
     When is only one --start then it is ignored.
@@ -80,16 +72,22 @@ if __name__ == "__main__":
         libpass.load_password_from_file(args.password_from_file)
     try:
         if args.start:
-            mkfifo = None
-            if args.mkfifo:
-                mkfifo = args.mkfifo
-                _named_fifo_path = mkfifo
+            server = None
+            if args.server:
+                server = args.server
             if all(isinstance(p, list) for p in args.start):
-                instances = start_multiple(args.start, **args_dict, mkfifo = mkfifo)
+                instances = start_multiple(args.start, **args_dict, server = server)
+                server = None
+                if isinstance(instances, tuple):
+                    instances = instances[0]
+                    server = instances[1]
                 for instance in instances:
                     instance.join()
             else:
                 instance = start(*args.start, **args_dict)
+                if isinstance(instances, tuple):
+                    instances = instances[0]
+                    server = instances[1]
                 instance.join()
     except TimeoutError as te:
         log.error(f"Timeout expired: {te}")
